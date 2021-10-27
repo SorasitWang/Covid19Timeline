dc17b633bb16d098ee8cc6fd06cf58a6704415d9
SorasitWang <sorasit789@gmail.com>
2021-10-27 13:22:56 +0700

    remove db info

--- connect.php
+++ connect.php
@@ -1,14 +1,14 @@
     
     <?php
-        $servername = 'localhost';
-        $username = 'id17832799_root';
-        $password = 'h[{X_[R^tX]1me]Z';
-        $dbname = 'id17832799_coide19timeline';
+        $servername = '';
+        $username = '';
+        $password = '';
+        $dbname = '';
         $conn = mysqli_connect($servername, $username, $password, $dbname);
         if (!$conn) {
-        	die("Connection : failed (เชื่อมต่อฐานข้อมูล ไม่ สำเร็จ)" . mysqli_connect_error());
+        	die("Connection : failed" . mysqli_connect_error());
         } else {
-        	//echo "Connection : OK (เชื่อมต่อฐานข้อมูลสำเร็จ)";
+        	//echo "Connection : OK";
         }
         
         ?>
